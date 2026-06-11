742eadf729e86c3eab1f464f773ea7a90e6c3d68
swapnil-janorkar <swapniljanorkar148@gmail.com>
2026-06-11 09:24:21 +0530

    dockerize backend and postgres services

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,12 +1,16 @@
 import express from "express";
+import { pool } from "./config/db";
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/", (_, res) => {
+app.get("/", async (_, res) => {
+    const r = await pool.query("SELECT NOW()");
+
     res.json({
-        message: "DIDO API Running"
+        message: "DIDO API Running",
+        dbTime: r.rows[0]
     });
 });
 
