3f5be90ff159ddae6d543b7a99031c0114d8c373
swapnil-janorkar <swapniljanorkar148@gmail.com>
2026-06-20 18:25:52 +0530

    add kubernetes health checks and rolling update support

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -10,6 +10,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/health", (req, res) => {
+    res.status(200).json({
+        status: "UP"
+    });
+});
+
 app.get("/", async (_, res) => {
     const r = await pool.query(
         "SELECT NOW()"
